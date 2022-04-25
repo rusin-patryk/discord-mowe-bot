35ec583d47a0b752f4896244f011d3e96b5f7aad
Patryk Rusin <rusin@patryk.in>
2022-04-25 19:19:00 +0200

    api route fix

--- api/routes/wnRoutes.js
+++ api/routes/wnRoutes.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
     const {getExpectedStats} = require('../controllers/wnController');
-    app.route('/api/wows/pr/expected')
+    app.route('/wows/pr/expected')
         .get(getExpectedStats)
 };
